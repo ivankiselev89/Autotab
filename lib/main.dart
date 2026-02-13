85def6892b62e9127b48fe306280a867bb23564b
ivankiselev89 <158662618+ivankiselev89@users.noreply.github.com>
2026-02-12 23:42:21 -0800

    Create main.dart file for Autotab Flutter app with theme and home screen setup

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Autotab',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Autotab Home'),
+      ),
+      body: Center(
+        child: Text('Welcome to Autotab!'),
+      ),
+    );
+  }
+}
